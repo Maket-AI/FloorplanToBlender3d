2c7f3f114b7d038b38cd8c2fb3cd0bacd744770a
DingLi23 <ding.li@mail.concordia.ca>
2024-02-27 14:34:48 -0500

    add local test

--- floorplan_detector/app.py
+++ floorplan_detector/app.py
@@ -16,17 +16,6 @@ except ImportError:
 from subprocess import check_output
 
 
-def local_test():
-    print(floorplan_lib_path)
-
-    image_names = ["floorplan_google2.jpg"]
-    for image_name in image_names:
-        input_image_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "events/", image_name))
-        save_image_path = os.path.abspath(os.path.join(os.path.dirname(__file__),"outputs", f"detected_{image_name}"))
-        # Call the processing function
-        return detect_floorplan_image(input_image_path, save_image_path, lambda_client)
-
-
 def lambda_handler(event, context):
     print(f"Event: {event}")
 
@@ -91,71 +80,3 @@ def lambda_handler(event, context):
     return {"statusCode": 200, "body": json.dumps({"response": response})}
 
 
-def process_batch_test(bucket_name, prefix):
-    # Initialize the S3 client
-    s3 = boto3.client('s3')
-
-    # List files in the specific S3 bucket and prefix
-    response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
-
-    # Extract file names from the response
-    files = [item['Key'] for item in response.get('Contents', [])]
-
-    # Initialize a dictionary to store the file names and paths
-    paths_dict = {}
-
-    for file_key in files:
-        # Check file extension
-        file_extension = file_key.lower().split('.')[-1]
-        if file_extension not in ['jpg', 'jpeg', 'png']:
-            # Skip files that are not in the desired format
-            print(f"Skipping file {file_key} as it is not a JPEG, PNG, or JPG.")
-            continue
-
-        # Construct the file URL
-        file_url = f"https://{bucket_name}.s3.ca-central-1.amazonaws.com/{file_key}"
-        
-        # Create an event for the lambda handler
-        event = {"image_url": file_url}
-        
-        # Call the lambda handler
-        response = lambda_handler(event, "")
-        body = json.loads(response['body'])
-
-        # Access the 'paths' key in the response
-        if 'paths' in body['response']:
-            paths_dict[file_key] = body['response']['paths']
-        else:
-            paths_dict[file_key] = ["No paths found in response."]
-    
-    return paths_dict
-
-
-def s3_test():
-    paths = []
-    image_file = ['test_1.png', 'test_2.png', 'test_3.jpg', 'test_4.jpg', 'test_5.png', 'test_6.jpeg']
-    image_file = ['floorplan_generator.png']
-    image_file = ['example.png']
-    print('start')
-    for image_name in image_file:
-        event = {
-            "image_url": f"https://floorplan-detector.s3.ca-central-1.amazonaws.com/2024-01-03/{image_name}"
-        }
-        event = {
-            "image_url":"https://floorplan-detector.s3.ca-central-1.amazonaws.com/uploaded-floorplan/floorplan-5fb4cd49-09ad-4431-bd5d-fc6f114951cb-645e2a67d62d1b49b4813c11.png"
-        }
-        response = lambda_handler(event, "")
-        body = json.loads(response['body'])
-
-        # Access the 'paths' key in the response
-        if 'paths' in body['response']:
-            paths.append(body['response']['paths'])
-        else:
-            paths.append(["No paths found in response."])
-    print(f"final response: {paths}")
-
-
-if __name__ == "__main__":
-    # paths_dict = process_batch_test('floorplan-detector', 'test_dataset/')
-    # print(f"final response: {paths_dict}")
-    s3_test()
